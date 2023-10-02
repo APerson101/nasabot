fe6a8f6f4ef25836462af53e3b6f520965ebbee0
Abdulhadi <abdulhadih48@gmail.co>
2023-10-02 14:08:18 +0100

    web build

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 from typing import Union
+import subprocess
 from run import (
   GetAnswer, 
    initialize )
@@ -12,10 +13,26 @@ def read_root():
     return {"Hello": "World"}
 
 
+def launch_flutter_app():
+    server_ip_address = "3.95.131.62"
+
+    # Replace "path/to/your/flutter/app" with the actual path to your Flutter app directory
+    flutter_app_directory = "~/nasabot/flutterapp"
+
+    # Change to the Flutter app directory
+    subprocess.run(["cd", flutter_app_directory], shell=True)
+
+    # Build the Flutter app for release
+    subprocess.run(["flutter", "build", "web", "--release"])
+
+    # Install and run the Flutter app on a connected device
+    subprocess.run(["flutter", "run", "--release", "-d", "all", "--dart-define=SERVER_IP=$server_ip_address"])
+    
 @app.on_event("startup")
 async def startup_event():
     print("FastAPI application starting up")
     initialize()
+    launch_flutter_app()
 
 
 
